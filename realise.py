dc8af35b868fbb6d0b381d8f6087fb4ea2bd860d
DK-droid <55908869+DK-droid@users.noreply.github.com>
2019-09-29 11:50:20 +0700

    Create realise.py

---
+++ realise.py
@@ -0,0 +1,24 @@
+from abstract import Subject as sbj
+from abstract import Observer as obs
+from abstract import DisplayElement as dsp
+
+
+class AppSLAE(sbj):
+    pass
+
+class Window(obs, dsp):
+    pass
+
+
+
+class mainwin(Window):
+    pass
+
+class calculator(Window):
+    pass
+
+class theory(Window):
+    pass
+
+class method(Window):
+    pass
